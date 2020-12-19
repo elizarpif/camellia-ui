efa7d5dbf6c65f36d050a413ac09068b543154f4
elizarpif <liza.pif@mail.ru>
2020-12-19 22:41:33 +0300

    formatted

--- camelia/camelia_test.go
+++ camelia/camelia_test.go
@@ -49,7 +49,6 @@ func Test_rotate128Key(t *testing.T) {
 	}
 }
 
-
 func fromHex(s string) []byte {
 	b, err := hex.DecodeString(s)
 	if err != nil {
@@ -58,26 +57,24 @@ func fromHex(s string) []byte {
 	return b
 }
 
-
-
 func TestVectors(t *testing.T) {
 	var vectors = []struct {
 		key, plaintext, ciphertext, name string
 	}{
 		{
-			name: "test1",
+			name:       "test1",
 			key:        "0123456789abcdeffedcba9876543210",
 			plaintext:  "0123456789abcdeffedcba9876543210",
 			ciphertext: "67673138549669730857065648eabe43",
 		},
 		{
-			name: "test3",
+			name:       "test3",
 			key:        "0123456789abcdeffedcba98765432100011223344556677",
 			plaintext:  "0123456789abcdeffedcba9876543210",
 			ciphertext: "b4993401b3e996f84ee5cee7d79b09b9",
 		},
 		{
-			name: "test2",
+			name:       "test2",
 			key:        "0123456789abcdeffedcba987654321000112233445566778899aabbccddeeff",
 			plaintext:  "0123456789abcdeffedcba9876543210",
 			ciphertext: "9acc237dff16d76c20ef7c919e3a7509",
@@ -100,6 +97,7 @@ func TestVectors(t *testing.T) {
 			if !bytes.Equal(ciphertext, buf) {
 				t.Fatalf("Test vector %d:\nEncryption failed\nFound:    %s\nExpected: %s", i, hex.EncodeToString(buf), hex.EncodeToString(ciphertext))
 			}
+
 			c.Decrypt(buf, buf)
 			if !bytes.Equal(plaintext, buf) {
 				t.Fatalf("Test vector %d:\nDecryption failed\nFound:    %s\nExpected: %s", i, hex.EncodeToString(buf), hex.EncodeToString(plaintext))
